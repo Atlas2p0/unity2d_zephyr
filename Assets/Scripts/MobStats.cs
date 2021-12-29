86cbad870d020d599414fbbc20c9ccd4e0a1c5aa
pseudness <pseudness@gmail.com>
2021-12-29 19:11:55 +0200

    Level2 Finalized

--- Assets/Scripts/MobStats.cs
+++ Assets/Scripts/MobStats.cs
@@ -41,7 +41,7 @@ public class MobStats : MonoBehaviour {
 		//Disable enemy
 		GetComponent<BoxCollider2D>().enabled = false;
 		GetComponent<CircleCollider2D>().enabled = false;
-		GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionY;
+		GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
 		GetComponent<Skeleton_Behaviour>().enabled = false;
 		skeletonTransform = GetComponent<Transform>();
 		this.enabled = false;
